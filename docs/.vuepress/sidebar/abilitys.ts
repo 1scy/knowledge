5614b0998ed827cf97d288caa1ce16404843a631
SuWeiMing <2363066934@qq.com>
2024-12-05 16:08:11 +0800

    修改排序

--- docs/.vuepress/sidebar/abilitys.ts
+++ docs/.vuepress/sidebar/abilitys.ts
@@ -1,19 +1,28 @@
 import { arraySidebar } from "vuepress-theme-hope";
 
 export const abilitys = arraySidebar([
+
   {
-    text: "docker容器部署",
-    link: "docker",
-    icon: "mdi:number-0-circle-outline",
-  },
-  {
-    text: "Nginx容器部署",
-    link: "docker-nginx",
-    icon: "mdi:number-1-circle-outline",
-  },
-  {
-    text: "Redis容器部署",
-    link: "docker-redis",
-    icon: "mdi:number-2-circle-outline",
+    text: "容器部署",
+    icon: "star",
+    collapsible: true,
+    prefix: "container/",
+    children: [
+      {
+        text: "docker容器部署",
+        link: "docker",
+        icon: "mdi:number-0-circle-outline",
+      },
+      {
+        text: "Nginx容器部署",
+        link: "docker-nginx",
+        icon: "mdi:number-1-circle-outline",
+      },
+      {
+        text: "Redis容器部署",
+        link: "docker-redis",
+        icon: "mdi:number-2-circle-outline",
+      },
+    ],
   },
 ]);
--- docs/.vuepress/sidebar/back-end-enact.ts
+++ docs/.vuepress/sidebar/back-end-enact.ts
@@ -1,9 +1,27 @@
 import { arraySidebar } from "vuepress-theme-hope";
 
 export const backEndEnact = arraySidebar([
+
   {
-    text: "Arthas（阿尔萨斯）",
-    link: "arthas",
-    icon: "logos:partytown-icon",
+    text: "SpringCloud",
+    icon: "star",
+    collapsible: true,
+    prefix: "SpringCloud/",
+    children: [
+    ],
+  },
+
+  {
+    text: "运维",
+    icon: "star",
+    collapsible: true,
+    prefix: "operations/",
+    children: [
+      {
+        text: "Arthas（阿尔萨斯）",
+        link: "arthas",
+        icon: "logos:partytown-icon",
+      },
+    ],
   },
 ]);
