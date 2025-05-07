061ce56a0116fc8b67b656eea0ea9ef292a0c97d
SuWeiMing <2363066934@qq.com>
2025-05-07 20:29:55 +0800

    docs(笔记碎片): 更新并发相关文档

--- docs/.vuepress/sidebar/笔记碎片.ts
+++ docs/.vuepress/sidebar/笔记碎片.ts
@@ -2,78 +2,82 @@ import {arraySidebar} from "vuepress-theme-hope";
 
 export const 笔记碎片 = arraySidebar([
     {
-        text: "ThreadPoolTaskExecutor",
+        text: "Java",
         icon: "experience",
         collapsible: true,
-        prefix: "/笔记碎片/并发/",
+        prefix: "/笔记碎片/Java/并发/",
         children: [
             {
-                text: "ThreadPoolTaskExecutor使用",
-                icon: "about",
-                link: "ThreadPoolTaskExecutor/ThreadPoolTaskExecutor使用.md",
+                text: "ThreadPoolTaskExecutor",
+                icon: "experience",
+                collapsible: true,
+                children: [
+                    {
+                        text: "ThreadPoolTaskExecutor使用",
+                        icon: "about",
+                        link: "ThreadPoolTaskExecutor/ThreadPoolTaskExecutor使用.md",
+                    },
+                ],
+            },
+            {
+                text: "@Async",
+                icon: "experience",
+                collapsible: true,
+                children: [
+                    {
+                        text: "@Async失效场景",
+                        icon: "about",
+                        link: "@Async/@Async失效场景.md",
+                    },
+                ],
+            },
+            {
+                text: "CompletableFuture",
+                icon: "experience",
+                collapsible: true,
+                children: [
+                    {
+                        text: "CompletableFuture创建异步任务",
+                        icon: "about",
+                        link: "CompletableFuture/CompletableFuture创建异步任务.md",
+                    },
+                    {
+                        text: "CompletableFuture异步回调处理",
+                        icon: "about",
+                        link: "CompletableFuture/CompletableFuture异步回调处理.md",
+                    },
+                    {
+                        text: "CompletableFuture多任务组合处理",
+                        icon: "about",
+                        link: "CompletableFuture/CompletableFuture多任务组合处理.md",
+                    },
+                ],
+            },
+            {
+                text: "ThreadLocal",
+                icon: "experience",
+                collapsible: true,
+                children: [
+                    {
+                        text: "ThreadLocal的简单使用",
+                        icon: "about",
+                        link: "ThreadLocal/ThreadLocal的简单使用.md",
+                    },
+                ],
+            },
+            {
+                text: "事务",
+                icon: "experience",
+                collapsible: true,
+                children: [
+                    {
+                        text: "TransactionSynchronizationManager使用",
+                        icon: "about",
+                        link: "TransactionSynchronizationManager/TransactionSynchronizationManager使用.md",
+                    },
+                ],
             },
         ],
     },
-    {
-        text: "@Async",
-        icon: "experience",
-        collapsible: true,
-        prefix: "/笔记碎片/并发/",
-        children: [
-            {
-                text: "@Async失效场景",
-                icon: "about",
-                link: "@Async/@Async失效场景.md",
-            },
-        ],
-    },
-    {
-        text: "CompletableFuture",
-        icon: "experience",
-        collapsible: true,
-        prefix: "/笔记碎片/并发/",
-        children: [
-            {
-                text: "CompletableFuture创建异步任务",
-                icon: "about",
-                link: "CompletableFuture/CompletableFuture创建异步任务.md",
-            },
-            {
-                text: "CompletableFuture异步回调处理",
-                icon: "about",
-                link: "CompletableFuture/CompletableFuture异步回调处理.md",
-            },
-            {
-                text: "CompletableFuture多任务组合处理",
-                icon: "about",
-                link: "CompletableFuture/CompletableFuture多任务组合处理.md",
-            },
-        ],
-    },
-    {
-        text: "ThreadLocal",
-        icon: "experience",
-        collapsible: true,
-        prefix: "/笔记碎片/并发/",
-        children: [
-            {
-                text: "ThreadLocal的简单使用",
-                icon: "about",
-                link: "ThreadLocal/ThreadLocal的简单使用.md",
-            },
-        ],
-    },
-    {
-        text: "事务",
-        icon: "experience",
-        collapsible: true,
-        prefix: "/笔记碎片/并发/",
-        children: [
-            {
-                text: "TransactionSynchronizationManager使用",
-                icon: "about",
-                link: "TransactionSynchronizationManager/TransactionSynchronizationManager使用.md",
-            },
-        ],
-    },
+
 ]);
